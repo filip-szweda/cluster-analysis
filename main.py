2cece494e6b694d26387bfa698fafb593bc1b118
FilipSzweda <filip.szweda1@gmail.com>
2022-03-24 01:10:55 +0100

    Add kmeans

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas
+from matplotlib import pyplot
+import numpy
 
 
-# Press the green button in the gutter to run the script.
+def kmeans(data, k):
+    x = data.values
+    diff = 1
+    cluster = numpy.zeros(x.shape[0])
+    centroids = data.sample(n=k).values
+    while diff:
+        for i, row in enumerate(x):
+            mn_dist = float('inf')
+            for idx, centroid in enumerate(centroids):
+                d = numpy.sqrt((centroid[0] - row[0]) ** 2 + (centroid[1] - row[1]) ** 2)
+                if mn_dist > d:
+                    mn_dist = d
+                    cluster[i] = idx
+        new_centroids = pandas.DataFrame(x).groupby(by=cluster).mean().values
+        if numpy.count_nonzero(centroids - new_centroids) == 0:
+            diff = 0
+        else:
+            centroids = new_centroids
+    return centroids, cluster
+
+
+def test_kmeans(data, country, k):
+    # right now only centroids are shown
+    centroids, cluster = kmeans(data, k)
+    for i in range(k):
+        pyplot.scatter(centroids[i][0], centroids[i][1], s=200, color='g')
+
+    pyplot.scatter(data.lat, data.lng, color='k')
+    pyplot.title("Concentration of cities in " + country + " as clasterized by kmeans")
+    pyplot.xlabel('Latitude')
+    pyplot.ylabel('Longitude')
+    pyplot.show()
+
+
+def main():
+    country = 'Poland'
+
+    data = pandas.read_csv('worldcities.csv', usecols=['lat', 'lng', 'country'])
+    data = data[data['country'] == country]
+    data.drop('country', inplace=True, axis=1)
+
+    test_kmeans(data, country, 4)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
